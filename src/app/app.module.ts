7a41515673a24e02d8c53c465746474c643571ee
Yuliia Melnyk <191982825+yulmelcorp@users.noreply.github.com>
2026-05-11 09:28:49 +0200

    feat: apply advanced overrides (#879)
    * feat: parse and apply css overrides, asyncTranslateLoader import changed,prettier formatter edits
    
    * fix: asyncTranslateLoader import changed to OnecxtranslateLoader after libs update
    
    * fix: removed parsing from shell, changed appending to the style element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,6 +32,7 @@ import { CurrentLocationTopic, EventsTopic, Theme, UserProfile } from '@onecx/in
 import {
   BASE_PATH,
   LoadWorkspaceConfigResponse,
+  OverrideType,
   UserProfileBffService,
   WorkspaceConfigBffService
 } from 'src/app/shared/generated'
@@ -51,6 +52,7 @@ import { PortalViewportComponent } from './shell/components/portal-viewport/port
 import { ParametersService } from './shell/services/parameters.service'
 import { mapSlots } from './shell/utils/slot-names-mapper'
 import { ImageRepositoryService } from './shell/services/image-repository.service'
+import { MARKED_AS_WRAPPED } from './shell/utils/styles/shared-styles-host-overwrites.utils'
 import { ShellIconLoaderService } from './shell/services/icon-loader.service'
 
 async function styleInitializer(
@@ -132,6 +134,7 @@ export async function workspaceConfigInitializer(
       string,
       Record<string, string>
     >
+
     const themeWithParsedProperties = {
       ...loadWorkspaceConfigResponse.theme,
       properties: parsedProperties
@@ -142,7 +145,7 @@ export async function workspaceConfigInitializer(
       routesService
         .init(loadWorkspaceConfigResponse.routes)
         .then(urlChangeListenerInitializer(router, appStateService)),
-      apply(themeService, themeWithParsedProperties),
+      applyThemeVariables(themeService, themeWithParsedProperties),
       remoteComponentsService.remoteComponents$.publish({
         components: loadWorkspaceConfigResponse.components,
         slots: mapSlots(loadWorkspaceConfigResponse.slots)
@@ -308,7 +311,7 @@ export function urlChangeListenerInitializer(router: Router, appStateService: Ap
   }
 }
 
-async function apply(themeService: ThemeService, theme: Theme): Promise<void> {
+export async function applyThemeVariables(themeService: ThemeService, theme: Theme): Promise<void> {
   console.log(`🎨 Applying theme: ${theme.name}`)
   await themeService.currentTheme$.publish(theme)
   if (theme.properties) {
@@ -317,6 +320,19 @@ async function apply(themeService: ThemeService, theme: Theme): Promise<void> {
         document.documentElement.style.setProperty(`--${key}`, value)
       }
     }
+  }
+  if (theme.overrides && theme.overrides.length > 0) {
+    theme.overrides
+      .filter((ov) => ov.type === OverrideType.CSS)
+      .forEach((override) => {
+        if (override.value) {
+          const el = document.createElement('style')
+          el.dataset['cssOverrides'] = ''
+          el.dataset[MARKED_AS_WRAPPED] = ''
+          el.append(override.value)
+          document.head.appendChild(el)
+        }
+      })
   }
 }
 
